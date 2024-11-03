8280ae8e53bbca8c9c92efe100e2f7e643c70c76
Melvin Burac <melvin_burac_3022@yahoo.com>
2024-11-03 15:58:31 +0800

    Install react and tailwind

---
+++ api/index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose
+.connect(process.env.mongoDBURL)
+.then(() => {
+    console.log('Connected to DB');   
+})
+.catch((err) => {
+    console.log(err);
+    
+});
+
+const app = express();
+
+app.listen(3000, () => {
+console.log('Server is running in port 3000');
+
+});
